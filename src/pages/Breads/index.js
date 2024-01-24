b2cfe8ad03952f58d020002bd9817b1aed6f5212
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-24 10:34:34 -0300

    25° commit - logica update implementada

--- src/pages/Breads/index.js
+++ src/pages/Breads/index.js
@@ -13,6 +13,15 @@ export default function Breads(){
     .then(response => {setBreads(response.data)})
   },[]);
 
+  // update, navega para pagina NewUpdate
+  async function updateBread(id){
+    try {
+      navigate(`/newupdate/${id}`);
+    } catch (error) {
+      alert('erro ao acessar NewUpdate');      
+    }
+  }
+
   return(
 
     <div data-testid="mycard" className="card border-primary" style={{marginTop: '20px'}} >
@@ -42,7 +51,8 @@ export default function Breads(){
                 <td>
 
                   <button data-testid="mybtn1" type="button"
-                  className="btn btn-outline-info" style={{margin: '2px'}}>Editar</button>
+                  className="btn btn-outline-info" style={{margin: '2px'}}
+                  onClick={() => updateBread(bread.id)}>Editar</button>
 
                   <button data-testid="mybtn2" type="button"
                   className="btn btn-outline-danger" style={{margin: '2px'}}>Excluir</button>
